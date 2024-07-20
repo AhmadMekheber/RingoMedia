26cf7f75c604b4622bdc1b0d7d0165cfd59f193a
Ahmad Mekheber <ahmad.y.mekheber@gmail.com>
2024-07-20 20:52:07 +0300

    Seeding Default Data

---
+++ Departments.Migrations/Migrations/20240720174132_SeedingDepartments.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Departments.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedingDepartments : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                "Departments",
+                columns: ["Name", "LogoPath", "ParentDepartmentID"],
+                values: new object[,] {
+                    {"Ringo HQ - US", "https://ringomedia.com/assets/img/logo-light.svg", null},
+                    {"Ringo HQ - EG", "https://ringomedia.com/assets/img/logo-light.svg", null},
+                    {"US Sub 1", "https://ringomedia.com/assets/img/logo-light.svg", 1},
+                    {"US Sub 2", "https://ringomedia.com/assets/img/logo-light.svg", 1},
+                    {"US Sub 1-1", "https://ringomedia.com/assets/img/logo-light.svg", 3},
+                    {"EG Sub 1", "https://ringomedia.com/assets/img/logo-light.svg", 2}
+                }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
